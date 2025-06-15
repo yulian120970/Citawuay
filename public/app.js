603b3a25ee12cbb10537887bad899ac091f25a8b
yulian120970 <yulianhernandotoledo@gmail.com>
2025-06-15 16:33:09 -0500

    Create app.js

---
+++ public/app.js
@@ -0,0 +1,11 @@
+// Servidor básico usando Express
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(port, () => {
+  console.log(`Citawuay corriendo en http://localhost:${port}`);
+});
